4881c21220efccd18a0c9c2d1d311bddf50f579a
Lucas Fernandes <lucasdanieldmx@gmail.com>
2025-03-24 15:48:53 -0400

    nao vai dartempo

---
+++ ex20.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+float x, y, z, caixa_total, custo_total, material, obra;
+    printf("Informe a altura: ");
+    scanf("%f", x);
+    printf("Informe a largura: ");
+    scanf("%f", y);
+    printf("Informe o comprimento: ");
+    scanf("%f", z);
+    caixa_total = ()+()+();
+
+    return 0;
+}
