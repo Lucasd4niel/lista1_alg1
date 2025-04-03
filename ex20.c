f09928fba86ee6b8e6d6f3a5ce3b90751584e26d
Lucas Fernandes <lucasdanieldmx@gmail.com>
2025-04-03 17:13:53 -0400

    deu certo pra carai

--- ex20.c
+++ ex20.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 int main(void){
-float x, y, z, caixa_total, custo_total, material, obra;
+int x, y, z, caixa_total, custo_total, ltinta, latas, rolo;
     printf("Informe a altura: ");
-    scanf("%f", x);
+    scanf("%d", &x);
     printf("Informe a largura: ");
-    scanf("%f", y);
+    scanf("%d", &y);
     printf("Informe o comprimento: ");
-    scanf("%f", z);
-    caixa_total = ()+()+();
-
+    scanf("%d", &z);
+    caixa_total = 2 * (x * y + x * z + y * z);
+    ltinta = 3 * caixa_total;
+    latas = (ltinta + 5 - 1) / 5;
+    rolo = (caixa_total + 10 - 1) / 10;
+    custo_total = latas * 45 + rolo * 5 + caixa_total * 20;
+    printf("%d\n", custo_total);
     return 0;
 }
