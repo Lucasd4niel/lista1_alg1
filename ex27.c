0ca6d9f756324922e288173f1dafb161e6b81e9f
Lucas Fernandes <lucasdanieldmx@gmail.com>
2025-04-09 10:12:23 -0400

    pourra

--- ex27.c
+++ ex27.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 int main(void){
-    int centavos;
+    int centavos, centavos_dividitos, conta_inteira;
     float carlos, andre, felipe, conta, cent_felipe;
     scanf("%f", &conta);
-    centavos = (conta * 100) / 3;
-    centavos = (centavos % 100)*3;
-    cent_felipe = centavos / 100;
-
-    carlos =  (conta / 3);
-    andre = (conta / 3);
-    felipe = (conta / 3) + cent_felipe;
-    printf("%f\n %f\n %f", carlos, andre, felipe);
+    centavos = (conta / 3) * 100;
+    centavos_dividitos = centavos % 100;
+    cent_felipe =  centavos_dividitos;
+    conta_inteira = (conta / 3);
+    cent_felipe = (cent_felipe /100)*3;
+    andre = conta_inteira;
+    carlos = conta_inteira;
+    felipe = conta_inteira + cent_felipe;
+    printf("%.2f\n %.2f\n %.2f\n", andre, carlos, felipe);
     return 0;
 }
